aef2cd042ecc45a9cae97c20cb538ffd2944389a
ermi-e3 <ermiyas_eshetu@outlook.com>
2026-06-20 09:39:09 +0300

    feat: registrar report route

--- Migrations/20260618070800_AddAgeFieldToStudentModel.cs
+++ Migrations/20260618070800_AddAgeFieldToStudentModel.cs
@@ -15,15 +15,14 @@ namespace TmsApi.Migrations
                 table: "Students",
                 type: "integer",
                 nullable: false,
-                defaultValue: 0);
+                defaultValue: 0
+            );
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropColumn(
-                name: "Age",
-                table: "Students");
+            migrationBuilder.DropColumn(name: "Age", table: "Students");
         }
     }
 }
